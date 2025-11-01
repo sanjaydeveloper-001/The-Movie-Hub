60302c341199ee65bc27582d8e4b5d9feb06b637
Sanjay <josephstudent001@gmail.com>
2025-11-01 15:03:14 +0530

    Removed unwanted Codes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import axios from "axios";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
